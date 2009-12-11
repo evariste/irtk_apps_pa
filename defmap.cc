421dfa850b018babfea9b65f24f2bbcee34126fc
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-12-11 14:36:36 +0000

    Added keywords and set property.

--- defmap.cc
+++ defmap.cc
@@ -1,3 +1,15 @@
+/*=========================================================================
+
+  Library   : Image Registration Toolkit (IRTK)
+  Module    : $Id$
+  Copyright : Imperial College, Department of Computing
+              Visual Information Processing (VIP), 2008 onwards
+  Date      : $Date$
+  Version   : $Revision$
+  Changes   : $Author$
+
+=========================================================================*/
+
 #ifdef HAS_VTK
 
 #include <irtkRegistration.h>
