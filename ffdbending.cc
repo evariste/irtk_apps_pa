df2246692e543dc5e301b1d73f3019456eb83955
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2010-02-24 20:39:24 +0000

    Incorporate an L operator norm.

--- ffdbending.cc
+++ ffdbending.cc
@@ -20,9 +20,15 @@ void usage()
 int main(int argc, char **argv)
 {
   int i, j, k, ok, count;
-  double x, y, z, bend, sumBend, padding;
-  double jac, sumJac, absLogJac, sumAbsLogJac, topPen, sumTopPen;
-  float *jacs, *bends, *absLogJacs, *topPens;
+  double x, y, z, padding;
+//   double jac, sumJac, bend, sumBend, absLogJac, sumAbsLogJac, topPen, sumTopPen;
+  double Lnorm, sumLnorm, sumLnormSq;
+//   float *jacs, *bends, *absLogJacs, *topPens;
+  float *Lnorms, *LnormsSq;
+
+  double alpha = 1;
+  double beta = 1;
+
 
   // Check command line
   if (argc < 3) {
@@ -40,7 +46,7 @@ int main(int argc, char **argv)
   // Initialize padding value
   padding = -1.0 * FLT_MAX;
 
- 
+
   while (argc > 1) {
     ok = False;
     if ((ok == False) && (strcmp(argv[1], "-padding") == 0)) {
@@ -50,11 +56,27 @@ int main(int argc, char **argv)
       argc--;
       argv++;
       ok = True;
-    }    
+    }
     if ((ok == False) && (strcmp(argv[1], "-mask") == 0)){
       argc--;
       argv++;
       mask_name = argv[1];
+      argc--;
+      argv++;
+      ok = True;
+    }
+    if ((ok == False) && (strcmp(argv[1], "-alpha") == 0)){
+      argc--;
+      argv++;
+      alpha = atof(argv[1]);
+      argc--;
+      argv++;
+      ok = True;
+    }
+    if ((ok == False) && (strcmp(argv[1], "-beta") == 0)){
+      argc--;
+      argv++;
+      beta = atof(argv[1]);
       argc--;
       argv++;
       ok = True;
@@ -68,10 +90,10 @@ int main(int argc, char **argv)
   // Read image
   irtkRealImage target;
   target.Read(target_name);
-  
+
   irtkGreyImage mask;
-  
-  // Set up a mask, 
+
+  // Set up a mask,
   if (mask_name == NULL){
     mask.Read(target_name);
 
@@ -83,7 +105,7 @@ int main(int argc, char **argv)
         *ptr2mask = 1;
       else
         *ptr2mask = 0;
-      
+
       ++ptr2tgt;
       ++ptr2mask;
     }
@@ -97,11 +119,11 @@ int main(int argc, char **argv)
       exit(1);
     }
   }
-  
+
   // Read transformation
   irtkMultiLevelFreeFormTransformation *mffd;
   mffd = new irtkMultiLevelFreeFormTransformation;
-  
+
   mffd->irtkTransformation::Read(dof_name);
 
   count = 0;
@@ -115,19 +137,25 @@ int main(int argc, char **argv)
     }
   }
 
-  jacs = new float[1 + count];
-  bends = new float[1 + count];
-  absLogJacs = new float[1 + count];
-  topPens = new float[1 + count];
-  
-  
-  sumBend = 0.0;
-  sumJac = 0.0;
-  sumAbsLogJac = 0.0;
-  sumTopPen = 0.0;
-  
+//  jacs = new float[1 + count];
+//  bends = new float[1 + count];
+//  absLogJacs = new float[1 + count];
+//  topPens = new float[1 + count];
+  Lnorms = new float[1 + count];
+  LnormsSq = new float[1 + count];
+
+//   sumBend = 0.0;
+//   sumJac = 0.0;
+//   sumAbsLogJac = 0.0;
+//   sumTopPen = 0.0;
+
+  sumLnorm = sumLnormSq = 0.0;
+
   count = 0;
-  
+
+  irtkMatrix jacMat(3,3);
+  double jacDet;
+
   for (k = 0; k < target.GetZ(); k++) {
     for (j = 0; j < target.GetY(); j++) {
       for (i = 0; i < target.GetX(); i++) {
@@ -138,46 +166,76 @@ int main(int argc, char **argv)
           z = k;
 
           target.ImageToWorld(x, y, z);
-          
-          bend = mffd->Bending(x, y, z);
-          jac = mffd->irtkTransformation::Jacobian(x, y, z);
-          absLogJac = fabs(log(jac));
-          topPen = log (0.5 * (jac*jac + 1.0 / (jac*jac)));
-          
-          sumBend += bend;
-          sumJac += jac;
-          sumAbsLogJac += absLogJac;
-          sumTopPen += topPen;
 
-          bends[1 + count] = bend;
-          jacs[1 + count] = jac;
-          absLogJacs[1 + count] = absLogJac;
-          topPens[1 + count] = topPen;
-          
-          
+//          bend = mffd->Bending(x, y, z);
+//          jac = mffd->irtkTransformation::Jacobian(x, y, z);
+//          absLogJac = fabs(log(jac));
+//          topPen = log (0.5 * (jac*jac + 1.0 / (jac*jac)));
+//
+//          sumBend += bend;
+//          sumJac += jac;
+//          sumAbsLogJac += absLogJac;
+//          sumTopPen += topPen;
+//
+//          bends[1 + count] = bend;
+//          jacs[1 + count] = jac;
+//          absLogJacs[1 + count] = absLogJac;
+//          topPens[1 + count] = topPen;
+
+
+          mffd->LocalJacobian(jacMat, x, y, z);
+
+          jacDet = jacMat.Det();
+          if (jacDet < 0.1 || jacDet > 5)
+            continue;
+
+
+          mffd->LocalDisplacement(x, y, z);
+
+          Lnorm = beta*beta * ((jacMat(0,0) * jacMat(0,0)) + 
+                               (jacMat(1,1) * jacMat(1,1)) +
+                               (jacMat(2,2) * jacMat(2,2)));
+
+          Lnorm += alpha*alpha * (x*x + y*y + z*z);
+
+          sumLnormSq += Lnorm;
+          LnormsSq[1 + count] = Lnorm;
+
+          Lnorm = sqrt(Lnorm);
+
+          sumLnorm += Lnorm;
+          Lnorms[1 + count] = Lnorm;
           ++count;
+
+
         }
       }
     }
   }
 
-  sort(count, bends);
-  sort(count, jacs);
-  sort(count, absLogJacs);
-  sort(count, topPens);
+//  sort(count, bends);
+//  sort(count, jacs);
+//  sort(count, absLogJacs);
+//  sort(count, topPens);
+  sort(count, Lnorms);
 
   i = 1 + (int) round(0.5 * (count - 1));
 
-  cout << sumBend / (double(count)) << " ";
-  cout << sumJac / (double(count)) << " ";
-  cout << sumAbsLogJac / (double(count)) << " ";
-  cout << sumTopPen / (double(count)) << " ";
+//  cout << sumBend / (double(count)) << " ";
+//  cout << sumJac / (double(count)) << " ";
+//  cout << sumAbsLogJac / (double(count)) << " ";
+//  cout << sumTopPen / (double(count)) << " ";
+  cout << sumLnorm / (double(count)) << " ";
+  cout << sumLnormSq / (double(count)) << " ";
 
-  cout << bends[i] << " ";
-  cout << jacs[i] << " ";
-  cout << absLogJacs[i] << " ";
-  cout << topPens[i] << " ";
+//  cout << bends[i] << " ";
+//  cout << jacs[i] << " ";
+//  cout << absLogJacs[i] << " ";
+//  cout << topPens[i] << " ";
+  cout << Lnorms[i] << " ";
+  cout << LnormsSq[i] << " ";
 
-  cout << count << endl;
+
+//   cout << count << endl;
 
 }
