355ea2d1feeb49127236d7ac3317308ce7c46c31
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2010-03-30 12:23:11 +0000

    Executable to use the scalars in a mesh as coordinates that replace the existing coordinates of the vertices.

--- tmp.cc
+++ tmp.cc
@@ -1,92 +1,80 @@
-////////////////////////////////////////////////////////////////////////
-// polydatasmooth with an option to stop when the L2 norm of H (extrinsic
-// curvature reaches a certain level indicating a threshold on smoothness..
-
-
 #if (defined HAS_VTK)
 
-#include <vtkFillHolesFilter.h>
-#include <vtkPolyDataSmoothCustom.h>
 #include <irtkImage.h>
-#include <nr.h>
 
+#include <vtkPolyData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPolyDataWriter.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+#include <vtkMath.h>
 
-
-
-char *input_name = NULL;
-char *output_name = NULL;
+char *input_name = NULL, *output_name = NULL;
+char *scalar_nameX = NULL;
+char *scalar_nameY = NULL;
+char *scalar_nameZ = NULL;
 
 void usage()
 {
+  cerr << "Usage: polydatascalars2coords [in] [out] [scalarX] [scalarY] [scalarZ] <options>" << endl;
   cerr << "" << endl;
-  cerr << " Usage:" << endl;
+  cerr << "Use the scalars of surface [in] as coordinates for the locations of its points." << endl;
+  cerr << "After applying the scalars, write the new surface to [out]." << endl;
+  cerr << "Surface in must have single component scalars with names [scalarX] [scalarY] [scalarZ]." << endl;
+  cerr << "" << endl;
+  cerr << "Options: " << endl;
+  cerr << "-norm radius   Normalise all coordinates to radius 1." << endl;
+  cerr << "-radius r      Normalise all coordinates to radius r." << endl;
+
   exit(1);
 }
 
-
 int main(int argc, char **argv)
 {
-  int ok, i, j, ind, noOfPoints;
-  int noOfIterations;
-  double relaxationFactor;
-  double smoothnessThreshold = -1.0f;
-  int trackingOn = False;
-  int lo, hi;
-  double val, minVal, maxVal;
-  double holeSize = 50;
-
-  lo = 2;
-  hi = 98;
-
-  if (argc < 4){
+  int ok;
+  int i, j;
+  int noOfPoints;
+  int ind;
+  double p[3], len;
+  int normalise = False;
+  double radius = 1.0;
+  
+  if (argc < 6){
     usage();
   }
 
-  // Parse arguments.
+  // Parse image
   input_name = argv[1];
-  argv++;
   argc--;
+  argv++;
   output_name = argv[1];
-  argv++;
   argc--;
-  noOfIterations = atoi(argv[1]);
   argv++;
+  scalar_nameX = argv[1];
   argc--;
-  relaxationFactor = atof(argv[1]);
   argv++;
+  scalar_nameY = argv[1];
   argc--;
+  argv++;
+  scalar_nameZ = argv[1];
+  argc--;
+  argv++;
 
+  
   // Parse remaining arguments
   while (argc > 1){
     ok = False;
-    if ((ok == False) && (strcmp(argv[1], "-track") == 0)){
+    if ((ok == False) && (strcmp(argv[1], "-norm") == 0)){
       argc--;
       argv++;
-      trackingOn = True;
+      normalise = True;
       ok = True;
     }
-    if ((ok == False) && (strcmp(argv[1], "-threshold") == 0)){
+    if ((ok == False) && (strcmp(argv[1], "-radius") == 0)){
       argc--;
       argv++;
-      smoothnessThreshold = atof(argv[1]);
-      argc--;
-      argv++;
-      ok = True;
-    }
-    if ((ok == False) && (strcmp(argv[1], "-holesize") == 0)){
-      argc--;
-      argv++;
-      holeSize = atof(argv[1]);
-      argc--;
-      argv++;
-      ok = True;
-    }    if ((ok == False) && (strcmp(argv[1], "-lohi") == 0)){
-      argc--;
-      argv++;
-      lo = atoi(argv[1]);
-      argc--;
-      argv++;
-      hi = atoi(argv[1]);
+      normalise = True;
+      radius = atof(argv[1]);
       argc--;
       argv++;
       ok = True;
@@ -97,193 +85,72 @@ int main(int argc, char **argv)
     }
   }
 
-  cerr << "Input        : " << input_name << endl;
-  cerr << "Output       : " << output_name << endl;
-  cerr << "Iterations   : " << noOfIterations << endl;
-  cerr << "Relax factor : " << relaxationFactor << endl;
 
-  // Read the polydata file
-  vtkPolyDataReader* reader = vtkPolyDataReader::New();
+  // Read surface
+  vtkPolyDataReader *reader = vtkPolyDataReader::New();
   reader->SetFileName(input_name);
+  reader->Update();
 
-  vtkPolyData* input = reader->GetOutput();
+  vtkPolyData *input = vtkPolyData::New();
+  input = reader->GetOutput();
   input->Update();
 
-  vtkPolyDataSmoothCustom *smoother = new vtkPolyDataSmoothCustom;
-  smoother->SetInput(input);
-  smoother->SetNoOfIterations(noOfIterations);
-  smoother->SetRelaxationFactor(relaxationFactor);
-  smoother->SetSmoothnessThreshold(smoothnessThreshold);
-  smoother->SetTrackingOn(trackingOn);
-  smoother->Run();
 
-  vtkPolyData *smoothedInput = smoother->GetOutput();
-  smoothedInput->Update();
+  noOfPoints= input->GetNumberOfPoints();
 
-  vtkCurvatures *curve = vtkCurvatures::New();
-  curve->SetInput(smoothedInput);
-  curve->SetCurvatureTypeToGaussian();
-  curve->Update();
+  vtkFloatArray *scalarsX, *scalarsY, *scalarsZ;
 
-
-  vtkPolyData *curveOutput = vtkPolyData::New();
-  curveOutput = curve->GetOutput();
-  curveOutput->Update();
-
-
-  vtkFloatArray *scalars;
-  scalars = (vtkFloatArray*) curveOutput->GetPointData()->GetArray("Gauss_Curvature", ind);
-
-  if (ind == -1 || scalars == NULL){
-    cerr << "Curvatures unavailable" << endl;
+  scalarsX = (vtkFloatArray*) input->GetPointData()->GetArray(scalar_nameX, ind);
+  if (ind == -1 || scalarsX == NULL){
+    cerr << "Scalars unavailable with name " << scalar_nameX << endl;
+    exit(0);
+  }
+  scalarsY = (vtkFloatArray*) input->GetPointData()->GetArray(scalar_nameY, ind);
+  if (ind == -1 || scalarsY == NULL){
+    cerr << "Scalars unavailable with name " << scalar_nameY << endl;
+    exit(0);
+  }
+  scalarsZ = (vtkFloatArray*) input->GetPointData()->GetArray(scalar_nameZ, ind);
+  if (ind == -1 || scalarsZ == NULL){
+    cerr << "Scalars unavailable with name " << scalar_nameZ << endl;
     exit(0);
   }
 
-  noOfPoints = curveOutput->GetNumberOfPoints();
-
-  float *data = new float[1 + noOfPoints];
-
-  for (i = 0; i < noOfPoints; ++i){
-    data[i+1] = scalars->GetTuple1(i);
+  if (scalarsX->GetNumberOfComponents() > 1){
+    cerr << "Scalars " << scalarsX->GetName() << " has more than one component." << endl;
+    exit(1);
+  }
+  if (scalarsY->GetNumberOfComponents() > 1){
+    cerr << "Scalars " << scalarsY->GetName() << " has more than one component." << endl;
+    exit(1);
+  }
+  if (scalarsZ->GetNumberOfComponents() > 1){
+    cerr << "Scalars " << scalarsZ->GetName() << " has more than one component." << endl;
+    exit(1);
   }
 
-  sort(noOfPoints, data);
-
-
-  i = 1 + (int) round( (double) lo * (noOfPoints - 1) / 100.0);
-  minVal = data[i];
-  i = 1 + (int) round( (double) hi * (noOfPoints - 1) / 100.0);
-  maxVal = data[i];
-
   for (i = 0; i < noOfPoints; ++i){
-    val = scalars->GetTuple1(i);
-    if (val < minVal || val > maxVal){
-      scalars->SetTuple1(i, 1);
-    } else {
-      scalars->SetTuple1(i, 0);
-    }
-  }
+    p[0] = scalarsX->GetTuple1(i);
+    p[1] = scalarsY->GetTuple1(i);
+    p[2] = scalarsZ->GetTuple1(i);
+    
+    if (normalise == True){
+      len = vtkMath::Norm(p);
 
-  //////////////////////////////////////
-  // Delete points:
-
-  vtkPoints *oldPoints = vtkPoints::New();
-  vtkPoints *newPoints = vtkPoints::New();
-
-  oldPoints = input->GetPoints();
-
-  int *newPtIds, *deletedPt;
-  int newPtCount;
-  double p[3];
-
-  newPtIds = new int[noOfPoints];
-  deletedPt = new int[noOfPoints];
-
-  for (i = 0; i < noOfPoints; ++i){
-    deletedPt[i] = 0;
-  }
-
-  newPtCount = 0;
-  for (i = 0; i < noOfPoints; ++i){
-
-    if (scalars->GetTuple1(i) > 0){
-      // Outside curvature limits. Ignore this point.
-      continue;
-    }
-    ++newPtCount;
-  }
-
-  newPoints->SetNumberOfPoints(newPtCount);
-
-  newPtCount = 0;
-  for (i = 0; i < noOfPoints; ++i){
-    if (scalars->GetTuple1(i) > 0){
-      deletedPt[i] = 1;
-    } else {
-      // Can keep the point.
-      oldPoints->GetPoint(i, p);
-      newPoints->InsertPoint(newPtCount, p);
-      newPtIds[i] = newPtCount;
-      ++newPtCount;
-    }
-  }
-
-  cerr << "Points in original surface: " << noOfPoints << endl;
-  cerr << "Points copied over        : " << newPtCount << endl;
-
-  ////////////////////////////////
-  // Delete faces.
-
-  vtkCellArray* oldFaces = vtkCellArray::New();
-  vtkCellArray* newFaces = vtkCellArray::New();
-
-  oldFaces = input->GetPolys();
-
-  int noOfFaces, newFaceCount, deleteFace, newId;
-  vtkIdType npts = 0;
-  vtkIdType *oldPtId;
-
-  noOfFaces = oldFaces->GetNumberOfCells();
-
-  newFaces->Initialize();
-  newFaces->Allocate(noOfFaces, 0);
-  newFaces->Reset();
-
-  oldFaces->InitTraversal();
-  newFaces->InitTraversal();
-  newFaceCount = 0;
-
-  for (i = 0; i < noOfFaces; ++i){
-    oldFaces->GetNextCell(npts, oldPtId);
-
-    deleteFace = 0;
-    for (j = 0; j < npts; ++j){
-      if(deletedPt[oldPtId[j]] == 1){
-        deleteFace = 1;
+      for (j = 0; j < 3; ++j){
+        p[j] = p[j] * radius / len;
       }
     }
 
-    if (deleteFace == 1)
-      continue;
-
-    newFaces->InsertNextCell(npts);
-    for (j = 0; j < npts; ++j){
-      newId = newPtIds[oldPtId[j]];
-      newFaces->InsertCellPoint(newId);
-    }
-    newFaces->UpdateCellCount(npts);
-    ++newFaceCount;
+    input->GetPoints()->SetPoint(i,p);
   }
-  newFaces->Squeeze();
-
-  cerr << "Faces in original surface : " << noOfFaces << endl;
-  cerr << "Faces copied over         : " << newFaceCount << endl;
-
-
-  /////////////////////////////////////////////////////
-
-
-  vtkPolyData* polyWithHoles = vtkPolyData::New();
-  polyWithHoles->SetPoints(newPoints);
-  polyWithHoles->SetPolys(newFaces);
-  polyWithHoles->Update();
-
-  vtkFillHolesFilter *holeFiller = vtkFillHolesFilter::New();
-  holeFiller->SetInput(polyWithHoles);
-  holeFiller->SetHoleSize(holeSize);
-
-
-
-  vtkPolyData* output = vtkPolyData::New();
-  output = holeFiller->GetOutput();
-  output->Update();
-
+  
+  input->Update();
 
   vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
-  writer->SetInput(output);
+  writer->SetInput(input);
   writer->SetFileName(output_name);
   writer->SetFileTypeToBinary();
-  writer->Update();
   writer->Write();
 
   return 0;
