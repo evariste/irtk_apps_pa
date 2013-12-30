ca29d898b2cd9640f533d4d6e74784b61a33b15c
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2013-12-30 05:49:51 +0000

    Code for adding concatenate_stacks.

--- applications/tmp.cc
+++ applications/tmp.cc
@@ -1,225 +1,255 @@
 
-#include <irtkGeometry.h>
-#include <irtkTransformation.h>
 
-// Default filenames
-char *rigid_in_name  = NULL;
-char *ffd_in_name  = NULL;
-char *ffd_out_name  = NULL;
+#include <irtkImage.h>
+#include <irtkFileToImage.h>
+
+char **input_names = NULL;
+char *tempStr = NULL;
+char *output_name = NULL;
+
 
 void usage()
 {
-  cerr << " Usage: thisApp rigidIn mffdIn mffdOut" << endl;
-  cerr << "  " << endl;
-  cerr << " Input set up: " << endl;
-  cerr << "  imageA ---------> ImageB --------> ImageC" << endl;
-  cerr << "          rigidIn           mffdIn" << endl;
-  cerr << "  " << endl;
-  cerr << " I.e. imageB is the target for mffdIn and the source for rigidIn. " << endl;
-  cerr << "  " << endl;
-  cerr << " Output: " << endl;
-  cerr << "  " << endl;
-  cerr << "  imageA -------------------------> ImageC " << endl;
-  cerr << "                  mffdOut" << endl;
-  cerr << "  I.e. the rigid transformation is incorporated into the input MFFD to give the output MFFD." << endl;
-  cerr << "  " << endl;
-
+  cerr << "Usage: " << endl;
+  cerr << "NAME_OF_EXE_HERE [output] <options>" << endl;
+  cerr << "" << endl;
+  cerr << "Options:" << endl;
+  cerr << "" << endl;
+  cerr << "-inputs input1 input2 ... inputN      Input stacks" << endl;
+  cerr << "-stack_number val                     Use a specific stack's coordinate system, " << endl;
+  cerr << "                                      where val is chosen from 1 ... N. Default is " << endl;
+  cerr << "                                      the central stack (odd N), N/2th stack (even N)." << endl;
+  cerr << "" << endl;
+  cerr << "" << endl;
   exit(1);
 }
 
 int main(int argc, char **argv)
 {
-  int ok;
-
-  // Check command line
-  if (argc < 4) {
+  if (argc < 2){
     usage();
   }
 
-  // Parse source and target images
-  rigid_in_name = argv[1];
-  argc--;
-  argv++;
-  ffd_in_name = argv[1];
-  argc--;
-  argv++;
-  ffd_out_name = argv[1];
+  int ok, i, j, k, n, noOfInputs, frameStackIndex;
+  bool readingInput;
+  double x1, y1, z1, x2, y2, z2, val;
+
+
+  noOfInputs = 0;
+  frameStackIndex = -1;
+
+
+  output_name = argv[1];
   argc--;
   argv++;
 
-
-
-  // Parse remaining parameters
   while (argc > 1) {
     ok = false;
-    if ((ok == false) && (strcmp(argv[1], "-something") == 0)) {
+    if ((ok == false) && (strcmp(argv[1], "-inputs") == 0)) {
       argc--;
       argv++;
-//      some_variable = argv[1];
-//      // or
-//      some_variable = atoi(argv[1]);
-//      // or
-//      some_variable = atof(argv[1]);
-//      // And possibly
-//      argc--;
-//      argv++;
+
+      readingInput = true;
+      while (noOfInputs < argc - 1 && readingInput){
+        noOfInputs += 1;
+        if (argv[noOfInputs][0] == '-'){
+          readingInput = false;
+          noOfInputs--;
+        }
+      }
+
+      input_names = new char*[noOfInputs];
+
+      for (i = 0; i < noOfInputs; i++){
+        input_names[i] = argv[1];
+        argc--;
+        argv++;
+      }
       ok = true;
     }
 
+    if ((ok == false) && (strcmp(argv[1], "-stack_number") == 0)){
+      argc--;
+      argv++;
+
+      frameStackIndex = atoi(argv[1]) - 1;
+
+      argc--;
+      argv++;
+
+      ok = true;
+    }
+
+
     if (ok == false) {
-      cerr << "Can not parse argument " << argv[1] << endl;
+      cerr << "Unknown option: " << argv[1] << endl;
       usage();
     }
   }
 
-
-  // imageA -------------> imageB ---------------> imageC
-  //         x -> Rx = y           y-> Ay + u(y)
-  // Or
-  //         x -> Rx              Rx-> ARx + u(Rx)
-  // I.e.
-  //            x -> ARx + u(Rx)
-  // In imageA terms:
-  //            x -> Bx + uu(x)
-  // So
-  //       B = AR  , uu(x) = u(Rx)
-
-  // R is a rigid transformation matrix.
-  // A is an affine global part of the MFFD and u(x) is the local displacement field represented by a FFD.
-
-
-  // Read input rigid.
-  irtkRigidTransformation *rigidTransf = new irtkRigidTransformation;
-
-  rigidTransf->irtkTransformation::Read(rigid_in_name);
-
-  // Read input MFFD
-  irtkMultiLevelFreeFormTransformation *mffd_in = new irtkMultiLevelFreeFormTransformation;
-  mffd_in->irtkTransformation::Read(ffd_in_name);
-
-
-  irtkBSplineFreeFormTransformation3D *ffd_in = dynamic_cast<irtkBSplineFreeFormTransformation3D *>(mffd_in->GetLocalTransformation(0));
-
-  // Should probably check that mffd_in does not have more than one ffd in it!
-
-  irtkMatrix mffd_in_global, rigidMat, invRigidMat, mffd_out_global;
-
-  // Matrix A
-  mffd_in_global = mffd_in->irtkAffineTransformation::GetMatrix();
-
-  // Matrix R
-  rigidMat = rigidTransf->GetMatrix();
-
-  invRigidMat = rigidMat;
-  invRigidMat.Invert();
-
-  // A*R
-  mffd_out_global = mffd_in_global * rigidMat;
-
-  double x1, x2, y1, y2, z1, z2;
-  double xaxis[3], yaxis[3], zaxis[3];
-  double dx, dy, dz;
-
-  ffd_in->GetSpacing(dx, dy, dz);
-
-  ffd_in->GetOrientation(xaxis, yaxis, zaxis);
-
-  // Get bounding box of input FFD
-  x1 = 0.0;
-  y1 = 0.0;
-  z1 = 0.0;
-  x2 = ffd_in->GetX() - 1;
-  y2 = ffd_in->GetY() - 1;
-  z2 = ffd_in->GetZ() - 1;
-
-  ffd_in->LatticeToWorld(x1, y1, z1);
-  ffd_in->LatticeToWorld(x2, y2, z2);
-
-  // Transform to get the bounding box of the output FFD
-  irtkVector v_in(4);
-  irtkVector v_out(4);
-
-  v_in(0) = x1;
-  v_in(1) = y1;
-  v_in(2) = z1;
-  v_in(3) = 1;
-
-  v_out = invRigidMat * v_in;
-
-  x1 = v_out(0);
-  y1 = v_out(1);
-  z1 = v_out(2);
-
-  v_in(0) = x2;
-  v_in(1) = y2;
-  v_in(2) = z2;
-  v_in(3) = 1;
-
-  v_out = invRigidMat * v_in;
-
-  x2 = v_out(0);
-  y2 = v_out(1);
-  z2 = v_out(2);
-
-  // Transform the the input axes to get the axes of the output FFD. Only need to rotate.
-
-  irtkMatrix rotMat = invRigidMat;
-  // Zero out the translation parameters.
-  rotMat(0, 3) = 0;
-  rotMat(1, 3) = 0;
-  rotMat(2, 3) = 0;
-
-  // Transform each axis in turn.
-
-  v_in(0) = xaxis[0];
-  v_in(1) = xaxis[1];
-  v_in(2) = xaxis[2];
-
-  v_out = rotMat * v_in;
-
-  xaxis[0] = v_out(0);
-  xaxis[1] = v_out(1);
-  xaxis[2] = v_out(2);
-
-  v_in(0) = yaxis[0];
-  v_in(1) = yaxis[1];
-  v_in(2) = yaxis[2];
-
-  v_out = rotMat * v_in;
-
-  yaxis[0] = v_out(0);
-  yaxis[1] = v_out(1);
-  yaxis[2] = v_out(2);
-
-  v_in(0) = zaxis[0];
-  v_in(1) = zaxis[1];
-  v_in(2) = zaxis[2];
-
-  v_out = rotMat * v_in;
-
-  zaxis[0] = v_out(0);
-  zaxis[1] = v_out(1);
-  zaxis[2] = v_out(2);
-
-  // Create the output FFD with the transformed control point lattice.
-  irtkBSplineFreeFormTransformation3D *ffd_out = new irtkBSplineFreeFormTransformation3D(x1, y1, z1, x2, y2, z2, dx, dy, dz, xaxis, yaxis, zaxis);
-
-  // Copy the control point parameters to the new FFD.
-  int noOfDofs;
-  int i;
-
-  noOfDofs = ffd_in->NumberOfDOFs();
-  for (i = 0; i < noOfDofs; ++i){
-    ffd_out->Put(i, ffd_in->Get(i));
+  if (noOfInputs < 2){
+    cerr << "At least 2 inputs should be given." << endl;
+    exit(1);
   }
 
-  // Put the output FFD and global transformation into a new Multi Level FFD object.
-  irtkMultiLevelFreeFormTransformation *mffd_out = new irtkMultiLevelFreeFormTransformation;
+  if (frameStackIndex < 0){
+    frameStackIndex = noOfInputs / 2;
+  }
 
-  mffd_out->PushLocalTransformation(ffd_out);
-  mffd_out->PutMatrix(mffd_out_global);
+  if (frameStackIndex > noOfInputs - 1){
+    cerr << "Stack number out of range: " << frameStackIndex + 1 << endl;
+    cerr << "Exceeds number of inputs." << endl;
+    frameStackIndex = noOfInputs / 2;
+  }
 
-  mffd_out->irtkTransformation::Write(ffd_out_name);
+  cout << "Using frame from stack number " << frameStackIndex + 1 << ": " << input_names[frameStackIndex] << endl;
 
+
+  cout << "Combining following stacks: " << endl;
+  for (i = 0; i < noOfInputs; i++)
+    cout << input_names[i] << endl;
+  cout << endl;
+
+  irtkFileToImage *frameStackReader = irtkFileToImage::New(input_names[frameStackIndex]);
+  irtkBaseImage *targetImg;
+  targetImg = frameStackReader->GetOutput();
+
+  irtkImageAttributes frameStackAttributes;
+
+  frameStackAttributes = targetImg->GetImageAttributes();
+
+  /*
+   * Assumptions are that the orientation of the axes of the input images are all the same.
+   * That the centres of the image grids all form a straight line in the patients coordinate system.
+   * That the line is parallel
+   * to the z axis of each of the image grids.
+   * That we can use the one of the image grids as a coordinate system for the
+   * combined data
+   * That the origin of the new grid coincides with the origin of the chosen stack.
+   * The axes also coincide and we take as many z-slices as needed 'up' and 'down' to encompass all the data.
+   * That as we loop over the data in memory for each stack, where one ends, the next one begins.
+   *
+   * There may be differences in contrast across different stacks.
+   */
+
+  int slicesBefore, slicesAfter;
+
+  slicesBefore = 0;
+  slicesAfter  = 0;
+  for (n = 0; n < noOfInputs; n++){
+
+    irtkFileToImage *reader = irtkFileToImage::New(input_names[n]);
+    irtkBaseImage *image = reader->GetOutput();
+
+    // Some checking.
+    if (image->GetX() != targetImg->GetX() || image->GetY() != targetImg->GetY()){
+      cerr << "All stacks must have the same in-plane dimensions." << endl;
+      exit(1);
+    }
+
+    if (image->GetT() > 1){
+      cerr << "Only implemented for 3D volumes. Given volume has at least 4 dimensions: " << input_names[n] << endl;
+      exit(1);
+    }
+
+    // Main purpose of loop:
+
+    if (n < frameStackIndex){
+      slicesBefore += image->GetZ();
+    }
+
+    if (n > frameStackIndex){
+      slicesAfter += image->GetZ();
+    }
+
+    // Collect the origins for the first pair of inputs to see if the direction of the stacks matches
+    // the direction of the main z axis. See below.
+    if (n == 0){
+      image->GetOrigin(x1, y1, z1);
+    }
+
+    if (n == 1){
+      image->GetOrigin(x2, y2, z2);
+    }
+
+  }
+
+  // Direction of stacks as estimated by the displacement of the origins from first to last:
+  x1 = x2 - x1;
+  y1 = y2 - y1;
+  z1 = z2 - z1;
+
+  // Inner product of the estimated direction with the z-axis of the stack used as a frame.
+  val = x1 * frameStackAttributes._zaxis[0] + y1 * frameStackAttributes._zaxis[1] + z1 * frameStackAttributes._zaxis[2];
+
+  if (val < 0){
+    // Stacks are ordered in opposite direction to z axis.
+    swap(slicesAfter, slicesBefore);
+  }
+
+  // Offset of origin.
+
+  double dispX, dispY, dispZ;
+
+  // z-axis components:
+  x1 = frameStackAttributes._zaxis[0];
+  y1 = frameStackAttributes._zaxis[1];
+  z1 = frameStackAttributes._zaxis[2];
+
+  val = sqrt(x1*x1 + y1*y1 + z1*z1);
+
+  if (val <= 0){
+    cerr << "Value error in norm of z axis." << endl;
+    exit(1);
+  }
+
+  x1 /= val;
+  y1 /= val;
+  z1 /= val;
+
+
+  // Required offset in mm.
+  val = 0.5 * (slicesAfter - slicesBefore) * targetImg->GetZSize();
+
+  dispX = val * x1;
+  dispY = val * y1;
+  dispZ = val * z1;
+
+
+  frameStackAttributes._xorigin += dispX;
+  frameStackAttributes._yorigin += dispY;
+  frameStackAttributes._zorigin += dispZ;
+
+  int zdimTotal;
+
+  zdimTotal = slicesBefore + slicesAfter + targetImg->GetZ();
+
+  irtkImageAttributes targetAttributes = frameStackAttributes;
+
+  targetAttributes._z = zdimTotal;
+  targetImg->Initialize(targetAttributes);
+  targetImg->Print();
+
+  int currZoffset = 0;
+
+  for (n = 0; n < noOfInputs; n++){
+    irtkFileToImage *reader = irtkFileToImage::New(input_names[n]);
+    irtkBaseImage *image = reader->GetOutput();
+
+    for (k = 0; k < image->GetZ(); ++k){
+      for (j = 0; j < image->GetY(); ++j){
+        for (i = 0; i < image->GetX(); ++i){
+          val = image->GetAsDouble(i,j,k);
+          targetImg->PutAsDouble(i, j, k + currZoffset, val);
+        }
+      }
+    }
+
+    currZoffset += image->GetZ();
+  }
+
+
+
+  targetImg->Write(output_name);
 
 }
