8150515e2d4b6503b62caec2a5ddc9e73b518128
evariste <paul.aljabar@gmail.com>
2015-06-15 08:48:15 +0100

    Allow an output polydata with area attribute on vertices.

--- applications/polydatasurfacearea.cc
+++ applications/polydatasurfacearea.cc
@@ -13,13 +13,21 @@
 #define MAXVALS 100
 
 char *input_name = NULL;
+char *output_name = NULL;
 
 void usage()
 {
   cerr << " polydatasurfacearea [input] <-options>" << endl;
   cerr << " " << endl;
+  cerr << " Print the area of the polydata file." << endl;
+  cerr << " " << endl;
   cerr << " Options:" << endl;
   cerr << " " << endl;
+  cerr << " -output [file] : Save a VTK polydata file with a scalar array on the vertices" << endl;
+  cerr << "                  showing the area. The output is a triangulated version" << endl;
+  cerr << "                  of the input. Each vertex receives the sum of 1/3 of the" << endl;
+  cerr << "                  areas of triangles it is a part of." << endl;
+  cerr << "                  " << endl;
   exit(1);
 }
 
@@ -29,7 +37,7 @@ int main(int argc, char **argv)
     usage();
   }
 
-  int i;
+  double val;
   bool ok;
 
   input_name  = argv[1];
@@ -38,12 +46,15 @@ int main(int argc, char **argv)
 
   while (argc > 1){
     ok = false;
-    if ((ok == false) && (strcmp(argv[1], "-XXX") == 0)){
+    if ((ok == false) && (strcmp(argv[1], "-output") == 0)){
       argc--;
       argv++;
-      // Do stuff and maybe argv++ etc.
+      output_name = argv[1];
+      argc--;
+      argv++;
       ok = true;
     }
+
     if (ok == false){
       cerr << "Can not parse argument " << argv[1] << endl;
       exit(1);
@@ -64,29 +75,46 @@ int main(int argc, char **argv)
   triFilter->SetInput(inputRead);
   triFilter->Update();
 
-  vtkPolyData *input = vtkPolyData::New();
-  input = triFilter->GetOutput();
-  input->Update();
+  vtkPolyData *pd = vtkPolyData::New();
+  pd = triFilter->GetOutput();
+  pd->Update();
 
-  vtkCellArray* facets = input->GetPolys();
+  vtkCellArray* facets = pd->GetPolys();
   vtkTriangle* facet = vtkTriangle::New();
 
   double A = 0.0;
   double v0[3], v1[3], v2[3];
 
+  vtkFloatArray *scalarsOut = vtkFloatArray::New();
+
+  scalarsOut->SetNumberOfComponents(1);
+  scalarsOut->SetNumberOfTuples(pd->GetNumberOfPoints());
+  scalarsOut->SetName("area");
+
   vtkIdType f, *vert=0;
   facets->InitTraversal();
   while (facets->GetNextCell(f,vert)){
 
-    input->GetPoint(vert[0],v0);
-    input->GetPoint(vert[1],v1);
-    input->GetPoint(vert[2],v2);
+    pd->GetPoint(vert[0],v0);
+    pd->GetPoint(vert[1],v1);
+    pd->GetPoint(vert[2],v2);
 
-    A += double(facet->TriangleArea(v0,v1,v2));
+    val = double(facet->TriangleArea(v0,v1,v2));
+    A += val;
 
-  }
+    for (int i = 0; i < 3; i++)
+      scalarsOut->SetTuple1(vert[i], val / 3.0);
+    }
 
   cout << A << endl;
+
+  if (output_name != NULL){
+    pd->GetPointData()->AddArray(scalarsOut);
+    vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
+    writer->SetFileName(output_name);
+    writer->SetInput(pd);
+    writer->Write();
+  }
 
 }
 
