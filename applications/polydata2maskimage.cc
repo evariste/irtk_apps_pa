a16a3cf74470d7af0ef46ba01929534b8c117dbc
evariste <paul.aljabar@gmail.com>
2015-10-11 18:59:35 +0100

    Complete re-write based on code in VTK examples.

--- applications/polydata2maskimage.cc
+++ applications/polydata2maskimage.cc
@@ -3,31 +3,28 @@
 
 #include <irtkImage.h>
 
-#include <irtkGaussianBlurring.h>
 
 #include <vtkPolyData.h>
 #include <vtkPolyDataReader.h>
-#include <vtkPolyDataWriter.h>
-#include <vtkPolyDataNormals.h>
 #include <vtkTriangleFilter.h>
-#include <vtkStripper.h>
 #include <vtkPolyDataToImageStencil.h>
 #include <vtkImageStencilData.h>
 #include <vtkImageStencil.h>
 #include <vtkImageData.h>
-#include <vtkTriangleFilter.h>
 
-char *input_polys = NULL, *output_name = NULL;
+#include <irtkTransformation.h>
+
+
+char *input_polys_name = NULL, *output_name = NULL;
 char *template_image_name = NULL;
 
 void usage()
 {
   cerr << "polydata2maskimage [polydata] [template image] [output image] <options>" << endl;
-  cerr << "-value val : value to put into the structure (default 1000)." << endl;
+  cerr << "-value val : value to put into the structure (default 255)." << endl;
   cerr << "-reverse   : toggle whether the structure or the background gets the value." << endl;
   cerr << "" << endl;
-  cerr << "See also: http://www.cmake.org/Wiki/VTK/Examples/Cxx/PolyData/PolyDataToImageData" << endl;
-  cerr << "for similar code." << endl;
+  cerr << "Based on code at www.cmake.org/Wiki/VTK/Examples/Cxx/PolyData/PolyDataToImageData" << endl;
 
   exit(1);
 }
@@ -35,17 +32,15 @@ void usage()
 int main(int argc, char **argv)
 {
   bool ok;
-  int n, i, j, k, xdim, ydim, zdim;
+  int i;
   irtkGreyPixel *ptr1;
-  short *ptr2;
-  short val;
-  int noOfPts;
-  double pt[3], newPt[3];
   double bounds[6];
-  int i1, j1, k1, i2, j2, k2;
-  double value = 1000;
-  int reverse = true;
+  short outValue = 1000;
+  int reverse = false;
   double xspacing, yspacing, zspacing;
+  double spacing[3];
+  int dim[3];
+  double minSpacing;
 
 
   if (argc < 4){
@@ -53,7 +48,7 @@ int main(int argc, char **argv)
   }
 
   // Parse parameters
-  input_polys = argv[1];
+  input_polys_name = argv[1];
   argv++;
   argc--;
   template_image_name = argv[1];
@@ -69,7 +64,7 @@ int main(int argc, char **argv)
     if ((ok == false) && (strcmp(argv[1], "-value") == 0)){
       argc--;
       argv++;
-      value = atof(argv[1]);
+      outValue = atoi(argv[1]);
       argc--;
       argv++;
       ok = true;
@@ -87,151 +82,180 @@ int main(int argc, char **argv)
     }
   }
 
+  ////////////////////////////////////////
+
+  // Read template image
+  irtkGreyImage templateImg;
+  templateImg.Read(template_image_name);
+
+
   // Read surface
-  vtkPolyDataReader *surface_reader = vtkPolyDataReader::New();
-  surface_reader->SetFileName(input_polys);
-  surface_reader->Modified();
-  surface_reader->Update();
-  vtkPolyData *inputRead = surface_reader->GetOutput();
+  vtkPolyDataReader *surfReader = vtkPolyDataReader::New();
+  surfReader->SetFileName(input_polys_name);
+  surfReader->Modified();
+  surfReader->Update();
 
   vtkTriangleFilter *triFilter = vtkTriangleFilter::New();
-  triFilter->SetInput(inputRead);
+  triFilter->SetInput(surfReader->GetOutput());
   triFilter->Update();
 
-
-  vtkPolyData* polys = vtkPolyData::New();
-  polys = triFilter->GetOutput();
-  polys->Update();
+  vtkPolyData *polys = triFilter->GetOutput();
 
 
+  // Image to be stenciled will have a smaller resolution than
+  // the template and will cover the bounds of the mesh.
 
-  // Read image
-  irtkGreyImage image, imageSubregion;
-  image.Read(template_image_name);
+  vtkSmartPointer<vtkImageData> whiteImage =
+    vtkSmartPointer<vtkImageData>::New();
 
-  noOfPts = polys->GetNumberOfPoints();
+  polys->GetBounds(bounds);
 
-  // What are the extents of the polydata in image coordinates?
-  i1 = image.GetX() - 1;
-  i2 = 0;
-  j1 = image.GetY() - 1;
-  j2 = 0;
-  k1 = image.GetZ() - 1;
-  k2 = 0;
-
-  for (i = 0; i < noOfPts; ++i){
-    polys->GetPoints()->GetPoint (i, pt);
-    image.WorldToImage(pt[0], pt[1], pt[2]);
-    if (i1 > pt[0])
-      i1 = (int) floor(pt[0]) - 1;
-    if (j1 > pt[1])
-      j1 = (int) floor(pt[1]) - 1;
-    if (k1 > pt[2])
-      k1 = (int) floor(pt[2]) - 1;
-    if (i2 < pt[0])
-      i2 = (int) floor(pt[0]) + 2;
-    if (j2 < pt[1])
-      j2 = (int) floor(pt[1]) + 2;
-    if (k2 < pt[2])
-      k2 = (int) floor(pt[2]) + 2;
+  char s[] = "xyz";
+  cout << "Polydata bounds: " << endl;
+  for (int i = 0; i < 3; i++){
+    cout << " " <<  s[i] << ": (" << bounds[i * 2] << ", " << bounds[i * 2+1] << ")" << endl;
   }
 
-  // Clamp.
-  i1 = max(i1, 0);
-  j1 = max(j1, 0);
-  k1 = max(k1, 0);
-  i2 = min(i2, image.GetX() - 1);
-  j2 = min(j2, image.GetY() - 1);
-  k2 = min(k2, image.GetZ() - 1);
+  // Use a spacing smaller than the minimum voxel spacing in the original image.
+  templateImg.GetPixelSize(&xspacing, &yspacing, &zspacing);
 
-  imageSubregion = image.GetRegion(i1, j1, k1, i2, j2, k2);
+  minSpacing = min(xspacing, yspacing);
+  minSpacing = min(minSpacing, zspacing);
 
-  irtkMatrix w2i = imageSubregion.GetWorldToImageMatrix();
+  spacing[0] = 0.3 * minSpacing;
+  spacing[1] = 0.3 * minSpacing;
+  spacing[2] = 0.3 * minSpacing;
+  whiteImage->SetSpacing(spacing);
 
-  xdim = imageSubregion.GetX();
-  ydim = imageSubregion.GetY();
-  zdim = imageSubregion.GetZ();
-
-  // Create a blank image where world and image coordinates coincide.
-  irtkGreyImage imageCanonical(xdim, ydim, zdim);
-  imageCanonical.PutOrigin((xdim - 1)/2.0, (ydim - 1)/2.0, (zdim - 1)/2.0);
-
-  ptr1 = imageCanonical.GetPointerToVoxels();
-  n = imageCanonical.GetNumberOfVoxels();
-  for (i = 0; i < n; ++i){
-    *ptr1 = 0;
-    ++ptr1;
+  // compute dimensions
+  for (int i = 0; i < 3; i++){
+    dim[i] = static_cast<int>(ceil((bounds[i * 2 + 1] - bounds[i * 2]) / spacing[i]));
   }
 
-  // Convert the polydata points to canonical image coordinates.
+  whiteImage->SetDimensions(dim);
+  whiteImage->SetExtent(0, dim[0] - 1, 0, dim[1] - 1, 0, dim[2] - 1);
 
-  for (i = 0; i < noOfPts; ++i){
-    polys->GetPoints()->GetPoint (i, pt);
-    newPt[0] = w2i(0, 0) * pt[0] + w2i(0, 1) * pt[1] + w2i(0, 2) * pt[2] + w2i(0, 3);
-    newPt[1] = w2i(1, 0) * pt[0] + w2i(1, 1) * pt[1] + w2i(1, 2) * pt[2] + w2i(1, 3);
-    newPt[2] = w2i(2, 0) * pt[0] + w2i(2, 1) * pt[1] + w2i(2, 2) * pt[2] + w2i(2, 3);
-    polys->GetPoints()->SetPoint(i, newPt);
+  // vtk origin is the voxel at image indices 0,0,0
+  double origVTK[3];
+  origVTK[0] = bounds[0] + spacing[0] / 2;
+  origVTK[1] = bounds[2] + spacing[1] / 2;
+  origVTK[2] = bounds[4] + spacing[2] / 2;
+
+  whiteImage->SetOrigin(origVTK);
+
+  whiteImage->SetScalarTypeToUnsignedChar();
+  whiteImage->AllocateScalars();
+
+  // fill the image with foreground voxels:
+  unsigned char fgVal = 255;
+  unsigned char bgVal = 0;
+
+  vtkIdType count = whiteImage->GetNumberOfPoints();
+
+  for (vtkIdType i = 0; i < count; ++i){
+    whiteImage->GetPointData()->GetScalars()->SetTuple1(i, fgVal);
   }
-  polys->Modified();
-  polys->Update();
 
-  // Convert image to VTK format to give to stencil.
-  vtkStructuredPoints *vtkimageIn = vtkStructuredPoints::New();
-  imageCanonical.ImageToVTK(vtkimageIn);
+  // polygonal data --> image stencil:
+  vtkSmartPointer<vtkPolyDataToImageStencil> pol2stenc =
+    vtkSmartPointer<vtkPolyDataToImageStencil>::New();
 
-  vtkPolyDataToImageStencil *dataToStencil = vtkPolyDataToImageStencil::New();
-  dataToStencil->SetTolerance(0.0);
-  dataToStencil->SetInput(polys);
+  pol2stenc->SetInput(polys);
 
-  dataToStencil->SetInformationInput(vtkimageIn);
+  pol2stenc->SetOutputOrigin(origVTK);
+  pol2stenc->SetOutputSpacing(spacing);
+  pol2stenc->SetOutputWholeExtent(whiteImage->GetExtent());
+  pol2stenc->Update();
 
+  // Cut the corresponding white image and set the background:
+  vtkSmartPointer<vtkImageStencil> imgstenc =
+    vtkSmartPointer<vtkImageStencil>::New();
 
-  vtkImageStencil *stencil = vtkImageStencil::New();
-  stencil->SetInput(vtkimageIn);
-  stencil->SetStencil(dataToStencil->GetOutput());
-  stencil->SetBackgroundValue(value);
+  imgstenc->SetInput(whiteImage);
+  imgstenc->SetStencil(pol2stenc->GetOutput());
 
   if (reverse == true){
-    stencil->ReverseStencilOn();
+    imgstenc->ReverseStencilOn();
   } else {
-    stencil->ReverseStencilOff();
+    imgstenc->ReverseStencilOff();
   }
 
+  imgstenc->SetBackgroundValue(bgVal);
+  imgstenc->Update();
 
-  vtkImageData *vtkimageOut = vtkImageData::New();
-  vtkimageOut = stencil->GetOutput();
-  vtkimageOut->Modified();
-  vtkimageOut->Update();
+  // Image to collect result at the higher resolution
+  vtkSmartPointer<vtkImageData> imgHiResVTK = vtkImageData::New();
 
-  // Retrieve the output in irtk canonical format.
-  n    = imageCanonical.GetNumberOfVoxels();
-  ptr1 = imageCanonical.GetPointerToVoxels();
-  ptr2 = (short *)vtkimageOut->GetScalarPointer();
-  for (i = 0; i < n; i++){
-    *ptr1 = *ptr2;
+  imgHiResVTK = imgstenc->GetOutput();
+  imgHiResVTK->Modified();
+  imgHiResVTK->Update();
+
+  // Attributes for IRTK nifti image.
+  irtkImageAttributes attr;
+  attr._t = 1;
+  attr._x = dim[0];
+  attr._y = dim[1];
+  attr._z = dim[2];
+
+  attr._dt = 1;
+  attr._dx = spacing[0];
+  attr._dy = spacing[1];
+  attr._dz = spacing[2];
+
+  attr._xorigin = origVTK[0] + (dim[0] - 1) * spacing[0] / 2.0;
+  attr._yorigin = origVTK[1] + (dim[1] - 1) * spacing[1] / 2.0;
+  attr._zorigin = origVTK[2] + (dim[2] - 1) * spacing[2] / 2.0;
+
+  // Retrieve data from stencil output to an IRTK image.
+  irtkGreyImage *imgHiRes = new irtkGreyImage(attr);
+
+  vtkTypeUInt8 *ptr2;
+
+  ptr1 = imgHiRes->GetPointerToVoxels();
+  ptr2 = (vtkTypeUInt8 *)imgHiResVTK->GetScalarPointer();
+
+  for (i = 0; i < imgHiRes->GetNumberOfVoxels(); i++){
+
+    if (*ptr2 > 0)
+      *ptr1 = outValue;
+
     ptr1++;
     ptr2++;
   }
 
-  // Prepare output image by clearing it.
-  n    = image.GetNumberOfVoxels();
-  ptr1 = image.GetPointerToVoxels();
-  for (i = 0; i < n; i++){
-    *ptr1 = 0;
+
+
+  // Transform the high resolution version onto the required template.
+  // Use linear interpolator.
+  irtkImageTransformation *imagetransformation =
+      new irtkImageTransformation;
+  irtkTransformation *transformation = new irtkRigidTransformation;
+  irtkImageFunction *interpolator = new irtkLinearInterpolateImageFunction;
+
+  imagetransformation->SetInput (imgHiRes, transformation);
+  imagetransformation->SetOutput(&templateImg);
+  imagetransformation->PutInterpolator(interpolator);
+  imagetransformation->Run();
+
+  // Threshold at 50%
+  int halfVal = outValue / 2;
+
+  ptr1 = templateImg.GetPointerToVoxels();
+  for (i = 0; i < templateImg.GetNumberOfVoxels(); i++){
+    if (*ptr1 >= halfVal){
+      *ptr1 = outValue;
+    } else {
+      *ptr1 = 0;
+    }
+
     ptr1++;
   }
 
-  // Paste the canonical data.
-    for (k = k1; k < k2; k++) {
-      for (j = j1; j < j2; j++) {
-        for (i = i1; i < i2; i++) {
-        	val = imageCanonical.Get(i-i1, j-j1, k-k1);
-        	image.Put(i, j, k, val);
-        }
-      }
-    }
 
-  image.Write(output_name);
+  templateImg.Write(output_name);
+
+
+
 }
 
 #else
@@ -243,97 +267,6 @@ int main( int argc, char *argv[] ){
 }
 
 #endif
-
-/// BEGIN GRAVEYARD:
-
-//   dataToStencil->SetInputConnection(stripper->GetOutputPort());
-//   dataToStencil->Update();
-
-//   stencil->Update();
-
-//   for (i = 0; i < 6; ++i){
-//     cout << bounds[i] << " ";
-//   }
-//   cout <<endl;
-
-//   cout << i1 << " " << j1 << " " << k1 << " " << i2 << " " << j2 << " " << k2 << endl;
-
-//   cout << "After transforming polys:" << endl;
-//   polys->GetBounds(bounds);
-//   for (int i = 0; i < 6; ++i){
-//     cout << bounds[i] << " ";
-//   }
-//   cout << endl;
-
-//   vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
-//   writer->SetInput(polys);
-//   writer->SetFileName("newPolys.vtk");
-//   writer->Write();
-
-//   image3.Write("image3.nii.gz");
-//   exit(0);
-
-//   image.Write("bla.nii.gz");
-
-
-//   cout << "vtkimage bounds:" << endl;
-//   vtkimage->GetExtent(i1, i2, j1, j2, k1, k2);
-//   cout << i1 << " " << i2 << " " << j1 << " " << j2 << " " << k1 << " " << k2 << endl;
-//   cout << endl;
-
-//   vtkTriangleFilter *triFilter = vtkTriangleFilter::New();
-//   triFilter->SetInput(polys);
-//   triFilter->Update();
-
-
-//   vtkStripper *stripper = vtkStripper::New();
-//   stripper->SetInput(triFilter->GetOutput());
-//   stripper->Update();
-
-//   cout << "vtkimageOut bounds:" << endl;
-//   vtkimageOut->GetExtent(i1, i2, j1, j2, k1, k2);
-//   cout << i1 << " " << i2 << " " << j1 << " " << j2 << " " << k1 << " " << k2 << endl;
-//   cout << endl;
-
-//   double scalarRange[2];
-//   vtkimageOut->GetScalarRange(scalarRange);
-//   cout << "vtkimageOut range : " << scalarRange[0] << " " << scalarRange[1] << endl;
-//   cout << "vtkimageOut ScalarTypeAsString : " << vtkimageOut->GetScalarTypeAsString() << endl;
-
-//   cout << "1" << endl;
-//   vtkImageStencilData *stencilData = vtkImageStencilData::New();
-
-//   double ox, oy, oz, dx, dy, dz;
-//   vtkimage->GetOrigin(ox, oy, oz);
-//   stencilData->SetOrigin(ox, oy, oz);
-//   vtkimage->GetSpacing(ox, oy, oz);
-//   stencilData->SetSpacing(ox, oy, oz);
-//   stencilData = dataToStencil->GetOutput();
-//   stencilData->UpdateData();
-//   cout << "2" << endl;
-
-//   int iter;
-//   int xmin, xmax, r1, r2;
-//   /// / Initialize the VTK image
-//   xmin = 0;
-//   xmax = xdim - 1;
-
-//   for (j = 0; j < ydim; ++j){
-//     for (k = 0; k < zdim; ++k){
-//       // Initialise iterator.
-//       iter = 0;
-//       stencilData->GetNextExtent(r1, r2, xmin, xmax, j, k, iter);
-//       while (iter != 0){
-//         for (i = r1; i <= r2; ++i){
-//           image.Put(i, j, k, 1);
-//         }
-//         stencilData->GetNextExtent(r1, r2, xmin, xmax, j, k, iter);
-//       }
-//     }
-//   }
-
-/// END GRAVEYARD
-
 
 
 
